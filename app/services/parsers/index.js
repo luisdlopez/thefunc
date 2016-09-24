011ee39baed5b1abbab96e100219273b3f416e12
luis doebbel <luis.doebbel@alayacare.com>
2016-09-24 17:15:28 -0400

    parsing moved to one thread;

--- app/services/parsers/index.js
+++ app/services/parsers/index.js
@@ -1,8 +1,13 @@
 'use strict';
 
 // TODO: allow for different language parsers
+import store from '../../vuex/store';
+import * as actions from '../../vuex/actions';
 let jsParser = require('./js');
 
-exports.parse = function parse (content) {
-  return jsParser.getFunctions(content);
+exports.parse = function parse (path, content) {
+  actions.startFileParsing(store, path);
+  actions.setFileFunctions(store, path, jsParser.getFunctions(content));
+  actions.endFileParsing(store, path);
+  return;
 };
--- app/services/scanner/index.js
+++ app/services/scanner/index.js
@@ -6,18 +6,36 @@
 // TODO: exclude folders as selected by user (node_modules as recommended)
 // TODO: handle errors!
 
-import store from '../../vuex/store';
-import * as actions from '../../vuex/actions';
+const fs = require('fs');
+const parsers = require('../parsers');
+// import store from '../../vuex/store';
+// import * as actions from '../../vuex/actions';
+
+function readChildren(child) {
+  if (child.children) {
+    child.children.forEach(readChildren);
+  }
+  else {
+    fs.readFile(child.path, 'utf8', function(error, content) {
+      if (error) console.error(`Error reading file: ${error.message}`);
+      else {
+        parsers.parse(child.path, content);
+        // try {
+        //   actions.startFileParsing(store, child.path);
+        //   actions.setFileFunctions(store, child.path, parsers.parse(content));
+        //   actions.endFileParsing(store, child.path);
+        // }
+        // catch(parsingError) {
+        //   actions.errorFileParsing(store, child.path);
+        // }
+      }
+    });
+
+  }
+}
 
 exports.parseDirectoryTree = function scanFolder (directoryTree) {
 
-  var cp = require('child_process');
-  var child = cp.fork('app/services/scanner/child-process.js');
-
-  child.send(directoryTree);
-
-  child.on('message', function({ action, params }) {
-    actions[action](store, ...params);
-  });
+  readChildren(directoryTree);
 
 };
